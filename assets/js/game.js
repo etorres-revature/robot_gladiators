340e8fa6bc9843d4008e7dbecce81cce1188e1bc
Eric D. Torres <etorresnotary@gmail.com>
2021-09-07 18:33:28 -0500

    robot variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,12 @@
 let playerRobotName = window.prompt("Please provide a name for your hunk of metal...")
+let playerHealth = 100;
+let playerAttack = 10;
 
-console.log(playerRobotName)
+console.log("PlayerName:", playerRobotName +";", "playerHealth", playerHealth +";", "; playerAttack", playerAttack+".")
+
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack = 12;
 
 const fight = () => {
     window.alert("The fight has commenced!!")
